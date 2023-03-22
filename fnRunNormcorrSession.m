6b5f0d7cdf641b91ae8897cb52273b721c2a04a5
marburyvthesea <jjm2128@gmail.com>
2023-03-22 16:13:10 -0500

    changes from 2p

--- fnRunNormcorrSession.m
+++ fnRunNormcorrSession.m
@@ -2,9 +2,10 @@ function [status] = fnRunNormcorrSession(pathToRecordings)
 % run caiman normcorr matlab function on a recording session directory
 
 fileList = dir(fullfile(pathToRecordings, '*converted.tif'));
+sizeFiles = size(fileList); 
+numFiles = sizeFiles(1, 1); 
 
-
-for i = 1:x
+for i = 1:numFiles
     
     quest_run_normcorr(fileList(i,1).folder, fileList(i,1).name); 
     
