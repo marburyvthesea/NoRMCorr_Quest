dc402d52f19cda4b4fb22c52b8673965661e274d
John Marshall <jjm2128@gmail.com>
2024-10-24 11:18:13 -0500

    from quest

--- groupMotionCorrectedFiles.m
+++ groupMotionCorrectedFiles.m
@@ -8,10 +8,10 @@ numFiles = sizeFiles(1, 1) ;
 for i = 1:group:numFiles
     if i+group-1 <= numFiles
         toGroup = fileList(i:i+group-1); 
-        concatenateFilesToh5_32bit(toGroup, strcat(num2str(i), '_', num2str(i+4), '_motioncorrected', '.h5'))
+        concatenateFilesToh5_32bit(toGroup, strcat(num2str(i), '_', num2str(i+4), '_motion*corrected', '.h5'))
     else 
         toGroup = fileList(i:numFiles);
-        concatenateFilesToh5_32bit(toGroup, strcat(num2str(i), '_', num2str(numFiles), '_motioncorrected','.h5'))
+        concatenateFilesToh5_32bit(toGroup, strcat(num2str(i), '_', num2str(numFiles), '_motion*corrected','.h5'))
     end
 end
 
