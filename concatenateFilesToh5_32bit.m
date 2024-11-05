0bcd3acea426ab6c3688a553f95ee21f03fadaf8
John Marshall <jjm2128@gmail.com>
2024-11-05 10:44:13 -0600

    changes from quest

--- concatenateFilesToh5_32bit.m
+++ concatenateFilesToh5_32bit.m
@@ -1,40 +1,48 @@
-function [file_path] = concatenateFilesToh5_32bit(files,filename)
+function [file_path] = concatenateFilesToh5_32bit(files, filename)
 
 % concatenate a list of files into a single file
 % INPUTS
-% files:        list of a files as a struct array. Name of each file is in files(i).name
+% files:        list of files as a struct array. Name of each file is in files(i).name
 % filename:     output filename
 
 % OUTPUTS
 
 numFiles = length(files);
-ln = zeros(numFiles, 1);
 
+% Read the first file and convert to float32
 Y1 = read_file(fullfile(files(1).folder, files(1).name));
-Y1_32bit = uint32(Y1);
+Y1_32bit = single(Y1);  % Convert to float32
 
-
-%%
 % File path for the HDF5 file
-file_path = fullfile(fullfile(files(1).folder, filename));
+file_path = fullfile(files(1).folder, filename);
 
 % Dataset name
 dataset_name = '/mov';
 
-% Saving to HDF5 file
-hdf5write(file_path, dataset_name, Y1_32bit);
-%%
-for i = 2:length(files)
-    % Load existing data from HDF5 file
-    existing_data = hdf5read(file_path, dataset_name);
-    % data to concat 
+% Get size of the data for creating the HDF5 dataset
+dataSize = size(Y1_32bit);
+
+% Create HDF5 file and write the first data set as float32
+h5create(file_path, dataset_name, [dataSize(1), dataSize(2), Inf], 'Datatype', 'single', 'ChunkSize', [dataSize(1), dataSize(2), 1]);
+
+% Write the first frame
+h5write(file_path, dataset_name, Y1_32bit, [1, 1, 1], dataSize);
+
+% Loop through the rest of the files
+for i = 2:numFiles
+    % Load the next file
     Yi = read_file(fullfile(files(i).folder, files(i).name));
-    Yi_32bit = uint32(Yi);
-    % Concatenate data
-    combined_data = cat(3, existing_data, Yi_32bit);
+    Yi_32bit = single(Yi);  % Convert to float32
 
-    % Write combined data back to the HDF5 file
-    hdf5write(file_path, dataset_name, combined_data);
+    % Get the size of the current data
+    dataSize = size(Yi_32bit);
 
+    % Get the current size of the dataset
+    info = h5info(file_path, dataset_name);
+    currentFrames = info.Datasets.Dataspace.Size(3);
+
+    % Write the new data, appending it to the existing dataset
+    h5write(file_path, dataset_name, Yi_32bit, [1, 1, currentFrames + 1], dataSize);
 end
+
 end
